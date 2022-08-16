ccb7021f65ef2488b3beaea003e83e7ba869fc6f
Michael Sphar <mikesphar@users.noreply.github.com>
2022-08-15 20:32:29 -0500

    initial commit

---
+++ demo.tf
@@ -0,0 +1,32 @@
+variable "compartment_ocid" {
+  default = "ocid1.compartment.oc1..aaaaaaaax4huh43m7va4t4lbcdmpmi22hjcdab3kbhj6vxjdeb55q4lqbklq"
+}
+
+variable "region" {
+  default = "ca-toronto-1"
+}
+
+variable "bucket_namespace" {
+  default = "terraform"
+}
+
+variable "bucket_name" {
+  default = "resource_manager_demo_bucket"
+}
+
+provider "oci" {
+  region = var.region
+}
+
+resource "oci_objectstorage_bucket" "create_bucket" {
+  compartment_id = var.compartment_ocid
+  name = var.bucket_name
+  namespace = var.bucket_namespace
+
+  access_type = "NoPublicAccess"
+}
+
+output "new_bucket" {
+  value = oci_objectstorage_bucket.create_bucket
+}
+
